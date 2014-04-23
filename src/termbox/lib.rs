f0496f27dce08e0f57b12f95eef3100cc2f6dc39
John Fresco <fresco.john@gmail.com>
2014-04-23 11:58:19 -0600

    Add more enums to wrap constants.

--- src/termbox/lib.rs
+++ src/termbox/lib.rs
@@ -17,3 +17,42 @@ pub fn init() -> int {
 pub fn shutdown() {
     unsafe { ffi::tb_shutdown() };
 }
+
+pub fn present() {
+    unsafe { ffi::tb_present() };
+}
+
+pub enum Mod {
+    Alt = ffi::TB_MOD_ALT
+}
+
+pub enum Color {
+    Default = ffi::TB_DEFAULT,
+    Black = ffi::TB_BLACK,
+    Red = ffi::TB_RED,
+    Green = ffi::TB_GREEN,
+    Yellow = ffi::TB_YELLOW,
+    Blue = ffi::TB_BLUE,
+    Magenta = ffi::TB_MAGENTA,
+    Cyan = ffi::TB_CYAN,
+    White = ffi::TB_WHITE
+}
+
+pub enum Attributes {
+    Bold = ffi::TB_BOLD,
+    Underline = ffi::TB_UNDERLINE,
+    Reverse = ffi::TB_REVERSE
+}
+
+pub enum EventType {
+    Key = ffi::TB_EVENT_KEY,
+    Resize = ffi::TB_EVENT_RESIZE
+}
+
+pub enum Output {
+    Current = ffi::TB_OUTPUT_CURRENT,
+    Normal = ffi::TB_OUTPUT_NORMAL,
+    _256 = ffi::TB_OUTPUT_256,
+    _216 = ffi::TB_OUTPUT_216,
+    Grayscale = ffi::TB_OUTPUT_GRAYSCALE
+}
